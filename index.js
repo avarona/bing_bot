a57f35d8c074305b873d5f6aa2c342d678ead8c1
Alejandro Varona <avarona10@gmail.com>
2020-05-04 20:28:50 -0400

    fix typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Builder } = require('selenium-webdriver');
 const { signIn, signOut } = require('./js/auth');
-const { getKeyWords, searchLoop } = require('./js/search');
+const { getKeywords, searchLoop } = require('./js/search');
 
 // Create browser instance
 const driver = new Builder().forBrowser('chrome').build();
@@ -15,7 +15,7 @@ const main = (async () => {
 		await signIn(driver, { E, P });
 		
 		// Fetch random words & start search loop
-		const keywords = await getKeyWords(30);
+		const keywords = await getKeywords(30);
 		await searchLoop(driver, keywords);
 		
 		// Sign out of user
