e0014fcfea66050b77149e5e5f31662f8ed9330d
Alejandro Varona <avarona10@gmail.com>
2020-05-15 11:38:20 -0400

    refactor rewards earned log

--- constants.js
+++ constants.js
@@ -8,5 +8,6 @@ module.exports = {
   XPATHS: {
     emailInputPath: '/html/body/div/form[1]/div/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/input[1]',
     passwordInputPath: '/html/body/div/form[1]/div/div/div[1]/div[2]/div[2]/div/div[2]/div/div[3]/div[2]/div/div/div/div/input',
+    rewardsShortPath: '//*[@id="home-app-host"]/div[1]/home-page/div[1]/home-banner/mee-banner/div/div/div/div[2]/div[2]/mee-banner-slot-3/banner-rewards/mee-progress-view/div[4]/finished-view/mee-banner-media/div/div[2]/media-body/p[1]',
   }
 }
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { signIn, signOut } = require('./js/auth');
 const { getKeywords, searchLoop } = require('./js/search');
 const { randomNumber, sleep } = require('./js/utils');
 
+const { URLS: { bingAccountURL }, XPATHS: { rewardsShortPath } } = require('./constants');
+
 const firefoxOptions = new firefox.Options().headless();
 const chromeOptions = new chrome.Options()
 	.addArguments('--no-sandbox')
@@ -19,25 +21,24 @@ const mobile = chrome.Driver.createSession(chromeOptions);
 const { E, P } = process.env;
 
 // Main script
-const main = async (driver, min, max, isBrowser) => {
+const main = async (driver, min, max) => {
 	try {
 		// Sign in to user
 		await signIn(driver, { E, P });
+		
+		// Calculate rewards before search
+		const rewardsBefore = await driver.findElement(By.xpath(rewardsShortPath)).getText();
+
 		// Fetch random words & start search loop
 		const count = await randomNumber(min, max);
 		const keywords = await getKeywords(count);
 		await searchLoop(driver, keywords);
-		// Rewards print
-		let rewardsCount;
-		if(isBrowser) {
-			await driver.wait(until.elementLocated(By.id('id_rc')));
-			rewardsCount = await driver.findElement(By.id('id_rc')).getText();
-		} else {
-			await driver.findElement(By.id('mHamburger')).click();
-			await driver.wait(until.elementLocated(By.id('fly_id_rc')));
-			rewardsCount = await driver.findElement(By.id('fly_id_rc')).getText();
-		}
-		console.log(`${rewardsCount} earned!`);
+
+		// Show rewards earned
+		await driver.get(bingAccountURL);
+		const rewardsAfter = await driver.findElement(By.xpath(rewardsShortPath)).getText();
+		console.log(`${rewardsBefore} -> ${rewardsAfter} earned!`);
+		
 		// Sign out of user
 		await signOut(driver);
 	} catch(err) {
@@ -51,13 +52,13 @@ const main = async (driver, min, max, isBrowser) => {
 (async () => {
 	try {
 		console.log('========== Starting mobile ==========');
-		await main(mobile, 20, 35, false);
+		await main(mobile, 20, 35);
 		console.log('Mobile done.');
 
 		await sleep(5000);
 
 		console.log('========== Starting browser ==========');
-		await main(browser, 30, 45, true);
+		await main(browser, 30, 45);
 		console.log('Browser done');
 	
 	} catch(err) {
