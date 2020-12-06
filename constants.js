4ec8ba0f7cb827bdf593b67c3400aec95aed50ef
Alejandro Varona <avarona10@gmail.com>
2020-12-06 17:57:26 -0500

    change password xpath

--- constants.js
+++ constants.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   XPATHS: {
     emailInputPath: '/html/body/div/form[1]/div/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/input[1]',
-    passwordInputPath: '/html/body/div/form[1]/div/div/div[1]/div[2]/div[2]/div/div[2]/div/div[3]/div[2]/div/div/div/div/input',
+    passwordInputPath: '/html/body/div/form[1]/div/div/div[1]/div[2]/div[2]/div/div[2]/div/div[2]/div/div[2]/input',
     rewardsShortPath: '//*[@id="userBanner"]/mee-banner/div/div/div/div[2]/div[1]/mee-banner-slot-2/mee-rewards-user-status-item/mee-rewards-user-status-balance/div/div/div/div/div/p[1]/mee-rewards-counter-animation/span',
   }
 }
